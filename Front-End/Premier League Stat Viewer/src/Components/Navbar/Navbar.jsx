c6d75079909e60630746d3609531cf8085810c3d
Jaden Toon <jt681@kent.ac.uk>
2025-06-16 18:49:08 +0100

    Changed project over to Vite and added TailWindCSS to project.

---
+++ Front-End/Premier League Stat Viewer/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+function Navbar(){
+    return(
+        <div className='navbar'>
+            <ul>
+                <li>Home</li>
+                <li>Clubs</li>
+                <li>Nationalities</li>
+                <li>Position</li>
+                <li>Search</li>
+            </ul>
+        </div>
+    );
+} export default Navbar
--- Front-End/Premier League Stat Viewer/src/Pages/Home/Home.jsx
+++ Front-End/Premier League Stat Viewer/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import premier_league_logo from '../../Assets/premier-league-logo.png';
+import Navbar from '../../Components/Navbar/Navbar';
 import './Home.css';
 
 function Home() {
     return(
         <div>
-            <div>
+            <Navbar className="nav"/>
+            <div className='textarea'>
                 <img src={premier_league_logo} alt="premier_league_logo" className='logo'/>
                 <h1>Welcome to the Premier League Stat Viewer</h1>
                 <p>Allowing you to look up all stats of your favourite players!</p>
