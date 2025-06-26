95f994906430fb3fa2b9ef7823e461d13f08d0be
Jaden Toon <jaden.toon@gmail.com>
2025-06-26 12:56:29 +0100

    Initialised Spring Boot Project.

---
+++ Back-End/Premier_League_Stats_Viewer/src/main/java/com/premier_league_stats/Premier_League_Stats_Viewer/PremierLeagueStatsViewerApplication.java
@@ -0,0 +1,13 @@
+package com.premier_league_stats.Premier_League_Stats_Viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierLeagueStatsViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierLeagueStatsViewerApplication.class, args);
+	}
+
+}
---
+++ Back-End/Premier_League_Stats_Viewer/src/test/java/com/premier_league_stats/Premier_League_Stats_Viewer/PremierLeagueStatsViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premier_league_stats.Premier_League_Stats_Viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierLeagueStatsViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
