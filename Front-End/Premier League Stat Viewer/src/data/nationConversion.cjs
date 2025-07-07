7d5977019e01f5b99b48e36aa2fc954711e71205
Jaden Toon <jaden.toon@gmail.com>
2025-07-07 18:53:17 +0100

    Finished script to get alpha2 codes for each Nation.

--- Front-End/Premier League Stat Viewer/src/data/nationConversion.cjs
+++ Front-End/Premier League Stat Viewer/src/data/nationConversion.cjs
@@ -5,11 +5,34 @@ const countries = require("i18n-iso-countries")
 
 countries.registerLocale(require("i18n-iso-countries/langs/en.json"))
 
+const manualCodes = {
+  ENG: { alpha2: "GB-ENG", name: "England" },
+  ALG: { alpha2: "DZ", name: "Algeria" },
+  SUI: { alpha2: "CH", name: "Switzerland" },
+  NED: { alpha2: "NL", name: "Netherlands" },
+  PAR: { alpha2: "PY", name: "Paraguay" },
+  DEN: { alpha2: "DK", name: "Denmark" },
+  GER: { alpha2: "DE", name: "Germany" },
+  URU: { alpha2: "UY", name: "Uruguay" },
+  NIR: { alpha2: "GB-NIR", name: "Northern Ireland" },
+  CHI: { alpha2: "CL", name: "Chile" },
+  WAL: { alpha2: "GB-WAL", name: "Wales" },
+  SCO: { alpha2: "GB-SCO", name: "Scotland" },
+  POR: { alpha2: "PT", name: "Portugal" },
+  BAN: { alpha2: "BD", name: "Bangladesh" },
+  ZAM: { alpha2: "ZM", name: "Zambia" },
+  CRO: { alpha2: "HR", name: "Croatia" },
+  GRE: { alpha2: "GR", name: "Greece" },
+  GAM: { alpha2: "GM", name: "Gambia" },
+  ZIM: { alpha2: "ZW", name: "Zimbabwe" },
+  KVX: { alpha2: "XK", name: "Kosovo" }
+};
 const uniqueCodes = new Set();
 
-const inputFile = path.join(__dirname, "../../../../players.csv");
+const inputPath = path.join(__dirname, "../../../../players.csv");
+const outputPath = path.join(__dirname, "nations.json");
 
-fs.createReadStream(inputFile)
+fs.createReadStream(inputPath)
     .pipe(csv())
     .on("data", (row) => {
         const nationCode3 = row.Nation?.trim().toUpperCase();
@@ -24,8 +47,11 @@ fs.createReadStream(inputFile)
             if (nationCode2 && nationName){
                 nationalities.push({nationCode3, nationCode2, nationName})
             } else {
-                console.warn(nationCode3+ " does not have an alpha 2 code.")
+                const nationCode2 = manualCodes[nationCode3].alpha2;
+                const nationName = manualCodes[nationCode3].name;
+                nationalities.push({nationCode3, nationCode2, nationName});
             }
         });
-        console.log(nationalities);
+        fs.writeFileSync(outputPath, JSON.stringify(nationalities, null, 2), "utf-8");
+        console.log("Successfully saved to nations.json!")
     })
