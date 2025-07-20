c9db8c28891f1e30496a50118bdbbfca0287ffc3
Jaden Toon <jaden.toon@gmail.com>
2025-07-20 21:57:44 +0100

    Updated clubs page and created clubs.json

--- Front-End/Premier League Stat Viewer/src/Pages/Clubs/Clubs.jsx
+++ Front-End/Premier League Stat Viewer/src/Pages/Clubs/Clubs.jsx
@@ -1,14 +1,24 @@
 import Navbar from '../../Components/Navbar/Navbar';
 import Searchbar from '../../Components/Searchbar/Searchbar';
 
-function Clubs(){
+function Clubs() {
     var clubs = ["arsenal", "aston villa", "chelsea"] //Placeholder
     return (
         <div>
-            <Navbar/>
-            <div className='relative z-0 min-h-screen px-4 py-20 space-y-8'>
-                <h1 className='text-3xl font-bold mb-4 py-5'>CLUBS</h1>
-                <Searchbar array={clubs}/>
+            <div>
+                <Navbar />
+                <div className='relative z-0 min-h-screen px-4 py-20 bg-gray-100'>
+                    <main className='max-w-7xl mx-auto space-y-8'>
+                        <header className='text-center'>
+                            <h1 className='text-4xl font-extrabold mt-4 text-gray-800'>Explore Clubs</h1>
+                            <p className='text-gray-500 mt-3'>Browse through all available clubs</p>
+                        </header>
+
+                        <div className='flex justify-center'>
+                            <Searchbar/>
+                        </div>
+                    </main>
+                </div>
             </div>
         </div>
     )
