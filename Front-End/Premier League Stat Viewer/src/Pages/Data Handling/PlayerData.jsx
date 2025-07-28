0ce1cc1b97ae084d86d8cefef8f65a8ae1f2639c
Jaden Toon <jaden.toon@gmail.com>
2025-07-28 21:10:05 +0100

    Improved interface for the player table page.

--- Front-End/Premier League Stat Viewer/src/Pages/Data Handling/PlayerData.jsx
+++ Front-End/Premier League Stat Viewer/src/Pages/Data Handling/PlayerData.jsx
@@ -6,80 +6,87 @@ import Navbar from '../../Components/Navbar/Navbar';
 const PlayerData = () => {
     const [playerData, setPlayerData] = useState([]);
     const [error, setError] = useState(null);
-    
+
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
         const squadData = params.get('squad');
         const nationData = params.get('nation');
         const posData = params.get('pos');
 
-        if(squadData){
+        if (squadData) {
             axios.get(`http://localhost:8081/api/v1/players?squad=${encodeURIComponent(squadData)}`)
-            .then(response => {
-                setPlayerData(response.data)
-            })
-            .catch(error => {
-                setError(error);
-            })
-        } else if (nationData){
+                .then(response => {
+                    setPlayerData(response.data)
+                })
+                .catch(error => {
+                    setError(error);
+                })
+        } else if (nationData) {
             axios.get(`http://localhost:8081/api/v1/players?nation=${encodeURIComponent(nationData)}`)
-            .then(response => {
-                console.log("Data: " ,response.data)
-                setPlayerData(response.data)
-            })
-            .catch(error => {
-                setError(error)
-            })
-        } else if (posData){
+                .then(response => {
+                    console.log("Data: ", response.data)
+                    setPlayerData(response.data)
+                })
+                .catch(error => {
+                    setError(error)
+                })
+        } else if (posData) {
             axios.get(`http://localhost:8081/api/v1/players?pos=${encodeURIComponent(posData)}`)
-            .then(response => {
-                setPlayerData(response.data)
-            })
-            .catch(error => {
-                setError(error)
-            })
-        } 
+                .then(response => {
+                    setPlayerData(response.data)
+                })
+                .catch(error => {
+                    setError(error)
+                })
+        }
     }, []);
 
     if (error) return <div>Error: {error.message}</div>;
     if (playerData.length === 0) return <div>Loading...</div>;
 
     return (
-        <div>
+        <div className='min-h-screen bg-gray-50'>
             <Navbar />
-            <div>
-                <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Nation</th>
-                        <th>Position</th>
-                        <th>Squad</th>
-                        <th>Age</th>
-                        <th>Born</th>
-                        <th>Matches Played</th>
-                        <th>Starts</th>
-                        <th>Minutes Played</th>
-                        <th>90s Played</th>
-                        <th>Goals</th>
-                    </tr>
-                    {playerData.map(player => (
-                        <tr key={player.id}>
-                            <th>{player.player}</th>
-                            <th>{player.nation}</th>
-                            <th>{player.pos}</th>
-                            <th>{player.squad}</th>
-                            <th>{player.age}</th>
-                            <th>{player.born}</th>
-                            <th>{player.matches_played}</th>
-                            <th>{player.starts}</th>
-                            <th>{player.min}</th>
-                            <th>{player.nineties_played}</th>
-                            <th>{player.goals}</th>
-                        </tr>
-                    ))}
-                </table>
-
-        </div>
+            <div className='flex justify-center items-center z-0 px-4 py-25'>
+                <div className='overflow-x-auto bg-white shadow-lg rounded-xl'>
+                    <table className='min-w-1/2 table-auto border-collapse '>
+                        <thead className='bg-gray-200 text-gray-700 uppercase text-sm'>
+                            <tr>
+                                <th className='px-3 py-2 text-left'>Name</th>
+                                <th className='px-3 py-2 text-left'>Nation</th>
+                                <th className='px-3 py-2 text-left'>Position</th>
+                                <th className='px-3 py-2 text-left'>Squad</th>
+                                <th className='px-3 py-2 text-left'>Age</th>
+                                <th className='px-3 py-2 text-left'>Born</th>
+                                <th className='px-3 py-2 text-left'>Matches Played</th>
+                                <th className='px-3 py-2 text-left'>Starts</th>
+                                <th className='px-3 py-2 text-left'>Minutes Played</th>
+                                <th className='px-3 py-2 text-left'>90s Played</th>
+                                <th className='px-3 py-2 text-left'>Goals</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {playerData.map((player,index) => (
+                                <tr 
+                                key={player.id}
+                                className={`border-b ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'} hover:bg-gray-100`}>
+                                    <td className='px-3 py-2'>{player.player}</td>
+                                    <td className='px-3 py-2'>{player.nation}</td>
+                                    <td className='px-3 py-2'>{player.pos}</td>
+                                    <td className='px-3 py-2'>{player.squad}</td>
+                                    <td className='px-3 py-2'>{player.age}</td>
+                                    <td className='px-3 py-2'>{player.born}</td>
+                                    <td className='px-3 py-2'>{player.matches_played}</td>
+                                    <td className='px-3 py-2'>{player.starts}</td>
+                                    <td className='px-3 py-2'>{player.min}</td>
+                                    <td className='px-3 py-2'>{player.nineties_played.toFixed(2)}</td>
+                                    <td className='px-3 py-2'>{player.goals}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
