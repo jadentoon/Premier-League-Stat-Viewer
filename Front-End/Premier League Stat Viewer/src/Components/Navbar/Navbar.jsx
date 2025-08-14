e4f97a108ec36cedccc4dca43a291d41ba0a792d
Jaden Toon <jaden.toon@gmail.com>
2025-08-15 00:52:27 +0100

    Added navbar highlighting for current page

--- Front-End/Premier League Stat Viewer/src/Components/Navbar/Navbar.jsx
+++ Front-End/Premier League Stat Viewer/src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,45 @@
 import { useState } from "react";
 import {HiMenuAlt3, HiOutlineX, HiOutlineUserGroup, HiOutlineHome, HiOutlineClipboardList , HiOutlineSearch, HiOutlineFlag} from "react-icons/hi";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar(){
 
     const navItems = [
-        {label: "Home", icon: <HiOutlineHome size="25"/>, route: "/"},
-        {label: "Clubs", icon: <HiOutlineUserGroup size="25"/>, route: "/clubs"},
-        {label: "Nationalities", icon: <HiOutlineFlag size="25"/>, route: "/nationality"},
-        {label: "Position", icon: <HiOutlineClipboardList  size="25"/>, route: "/position"},
-        {label: "Search", icon: <HiOutlineSearch size="25"/>, route: "/search"},
+        {label: "Home", icon: HiOutlineHome, route: "/"},
+        {label: "Clubs", icon: HiOutlineUserGroup, route: "/clubs"},
+        {label: "Nationalities", icon: HiOutlineFlag, route: "/nationality"},
+        {label: "Position", icon: HiOutlineClipboardList, route: "/position"},
+        {label: "Search", icon: HiOutlineSearch, route: "/search"},
     ]
 
     const [nav, setNav] = useState(false)
     const handleNav = () => setNav((prev) => !prev)
     const [hover, setHover] = useState(null);
 
+    const location = useLocation();
+
     return(
         <>
             <div className='flex justify-between items-center w-full py-[10px] px-[10%] fixed gap-12 bg-purple-300 drop-shadow-xl z-50'>
                 <ul className='hidden md:flex w-full justify-between'>
-                    {navItems.map((item, index) => (
-                        <li
-                        key={item.label}
-                        className="p-4 cursor-pointer text-md w-20 flex justify-center"
-                        onMouseEnter={() => setHover(index)}
-                        onMouseLeave={() => setHover(null)}
-                        >
-                            <Link to={item.route}>
-                                {hover === index ? item.label :item.icon}
-                            </Link>
-                        </li>
-                    ))}
+                    {navItems.map((item, index) => {
+                        const Icon = item.icon
+                        const isActive = location.pathname === item.route;
+                        return (
+                            <li
+                            key={item.label}
+                            className={`p-4 cursor-pointer text-md w-20 flex justify-center `}
+                            onMouseEnter={() => setHover(index)}
+                            onMouseLeave={() => setHover(null)}
+                            >
+                                <Link to={item.route}>
+                                    {hover === index ?
+                                     item.label : 
+                                     <Icon size={25} color={isActive ? "#9333EA" : "black"}/>}
+                                </Link>
+                            </li>
+                        )
+                    })}
                 </ul>
                 <div onClick={handleNav} className='block md:hidden cursor-pointer px-[100%]'>
                     {nav ? <HiOutlineX size={20}/> : <HiMenuAlt3 size={20}/>}
@@ -49,16 +57,19 @@ function Navbar(){
                 nav ? 'translate-x-0' : '-translate-x-full'
             }`}>
                 <ul className='uppercase p-4 mt-12 space-y-12'>
-                    {navItems.map((item) => (
-                        <li
-                        key={item.label}
-                        className='flex items-center gap-3 p-4 border-b border-gray-600 hover:bg-purple-400 cursor-pointer transition-colors duration-300'>
-                            {item.icon}
-                            <Link to={item.route}>
-                                {item.label}
-                            </Link>
-                        </li>
-                    ))}
+                    {navItems.map((item) => {
+                        const Icon = item.icon;
+                        return (
+                            <li
+                            key={item.label}
+                            className='flex items-center gap-3 p-4 border-b border-gray-600 hover:bg-purple-400 cursor-pointer transition-colors duration-300'>
+                                <Icon size={25}/>
+                                <Link to={item.route}>
+                                    {item.label}
+                                </Link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
         </>
